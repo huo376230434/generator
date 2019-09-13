ed0f9ba6ee87b575532f52c78c55cbd3858f1caf
huo376230434 <415384303@qq.com>
2019-09-13 23:27:30 +0800

    Update GeneratorServiceProvider.php

--- src/GeneratorServiceProvider.php
+++ src/GeneratorServiceProvider.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Str;
 
 class GeneratorServiceProvider extends ServiceProvider
 {
-    //hhhh asdf
+    //bu nty b 
     use GeneratorServiceProviderTrait;
     protected $namespace_prefix = "Huojunhao\Generator\DwMake\\";
 
